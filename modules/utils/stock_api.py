99d3282ef748dc0b4448aeed7717e0125b5f0119
dongju <smturtle2@kakao.com>
2025-11-07 16:39:32 +0900

    chore: defeatbeta-api 삭제 및 Alphavantage api 테스트 코드 추가

--- modules/utils/stock_api.py
+++ modules/utils/stock_api.py
@@ -1,9 +1,5 @@
 ﻿# modules/utils/stock_api.py
 import yfinance as yf
-from defeatbeta_api.data.ticker import Ticker as dfTicker
-
-def fetch_stock_data_dfa(ticker, start_date, end_date, interval="1m"):
-    data = dfTicker(ticker).price()
 
 def fetch_stock_data_yf(ticker, period="1d", interval="1m", end=None):
     stock = yf.Ticker(ticker)
@@ -12,12 +8,20 @@ def fetch_stock_data_yf(ticker, period="1d", interval="1m", end=None):
 
 
 def fetch_stock_data(ticker, period="1d", interval="1m", end=None):
-    stock = yf.Ticker(ticker)
-    data = stock.history(period=period, interval=interval, end=end)
+    data = fetch_stock_data_yf(ticker, period=period, interval=interval, end=end)
     return data
 
 
 if __name__ == "__main__":
-    ticker = "005930.KS"
+    ticker = "AAPL"
     data = fetch_stock_data(ticker)
     print(data)
+
+import requests
+
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+url = 'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey=demo'
+r = requests.get(url)
+data = r.json()
+
+print(data)
