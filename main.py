f13a435f3d411a407581569bf829dc137603edf2
dongju <smturtle2@kakao.com>
2025-11-17 23:54:03 +0900

    test: RankAgent 테스트

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 ﻿# main.py
-from graphs.analyst.graph import create_analyst_graph
+from graphs.rank import create_rank_graph
 from modules.context import Context
 
 context = Context()
-context.set_cache(ticker="AAPL")
-context.set_config(analysis_tasks = ["financial",])
-graph = create_analyst_graph(context)
-print()
-graph.run(context)
-print()
-print(context.get_report("financial"))
+context.set_config(
+    analysis_tasks=["financial",],
+    tickers=["AAPL", "GOOGL", "MSFT", "AMZN", "TSLA"],
+    max_portfolio_size=3,
+)
+
+context.set_cache(
+    date="20251101T0000",
+)
+
+rank_graph = create_rank_graph()
+
+rank_graph.run(context)
