6d74e63c5a07c3ad35ed3e4d77089eb9d1b65d4f
dongju <smturtle2@kakao.com>
2025-11-17 23:53:29 +0900

    feat: RankAgent 추가

---
+++ graphs/rank/schema.py
@@ -0,0 +1,7 @@
+﻿# graphs/analyst/schema.py
+from pydantic import BaseModel, Field
+from typing import Literal
+
+class ComparisonSchema(BaseModel):
+    reasoning: str = Field(..., description="Detailed reasoning process comparing the two stocks.")
+    better_stock: Literal['A', 'B'] = Field(..., description="The ticker symbol of the stock that is determined to be more promising based on the analysis.")
