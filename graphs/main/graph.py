02ec3d9ad84af75ff4eb7a83d6b452aea215293f
dongju <smturtle2@kakao.com>
2025-11-19 18:49:30 +0900

    refactor: create_main_graph 함수 개선 및 그래프 노드 생성 방식 수정

--- graphs/main/graph.py
+++ graphs/main/graph.py
@@ -1,17 +1,17 @@
 ﻿# graphs/main/graph.py
 from modules.graph import Graph
-from modules.context import Context
 from graphs.analyst import create_analyst_graph
 from graphs.rank import create_rank_graph
 from graphs.debate.factory import create_debate_graph
 
 
 def create_main_graph() -> Graph:
-    rank_graph = create_rank_graph()
-    analyst_graph = create_analyst_graph()
-    debate_graph = create_debate_graph()
 
-    graph = Graph("main_graph", start_node=rank_graph)
+    graph = Graph("main_graph", start_node=create_rank_graph())
+    rank_graph = graph.start_node.name
+    analyst_graph = graph.add_node(create_analyst_graph())
+    debate_graph = graph.add_node(create_debate_graph())
+    
     graph.add_edge(rank_graph, analyst_graph)
     graph.add_edge(analyst_graph, debate_graph)
 
